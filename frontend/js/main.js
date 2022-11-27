9656ca241701bdd81be5db7f73a42f7e4dae2460
Your Name <you@example.com>
2022-11-27 23:00:24 +0500

    portfoliyo

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -25,3 +25,34 @@ function linkAction(){
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/* Scrool reveal animation */
+const sr = ScrollReveal({
+    origin: 'top',
+    ditance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+/* Scrool home */
+sr.reveal('.home__title', {})
+sr.reveal('.button', {delay: 200})
+sr.reveal('.home__img', {delay: 400})
+sr.reveal('.home__social-icon', {interval: 200})
+
+/* Scroll about */
+sr.reveal('.about__img', {})
+sr.reveal('.about__subtitle', {delay: 200})
+sr.reveal('.about__text', {delay: 400})
+
+/* Scroll skills */
+sr.reveal('.skills__subtitle', {})
+sr.reveal('.skills__text', {delay: 200})
+sr.reveal('.skills__data', {interval: 200})
+sr.reveal('.skills__img', {delay: 400})
+
+/* Scroll work */
+sr.reveal('.work__img', {interval: 200})
+
+/* Scroll contact */
+sr.reveal('.contact__input', {interval: 200})
